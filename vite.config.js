c3cbb90f4508e7ea32cfe1bc21f4b2137e8a3621
María del Carmen <jacqueelopez@gmail.com>
2025-02-06 12:32:15 +0100

    fix style

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/modulo-3-evaluacion-final-jacquelinlopez",
   server:{
     watch:{
       usePolling: true,
